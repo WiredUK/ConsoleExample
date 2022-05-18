3afb66bab29dbff52a3d3b1404b71081ec412769
David Glass <dglass@allego.com>
2022-05-18 10:10:27 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿var arguments = ParseArguments(args);
+
+foreach(var argument in arguments)
+{
+    Console.WriteLine($"You specified the argument {argument.ArgumentType} with data of '{argument.Data}'");
+}
+
+
+
+
+IEnumerable<Argument> ParseArguments(string[] args)
+{
+    foreach(var arg in args)
+    {
+        var parts = arg.Split('=');
+
+        yield return new Argument
+        (
+            ParseArgumentName(parts[0]),
+            parts.Length > 1 ? parts[1] : ""
+        );
+    }
+
+}
+
+ArgumentType ParseArgumentName(string argument)
+{
+    return argument switch
+    {
+        "/uninstall" => ArgumentType.Uninstall,
+        "/quiet" => ArgumentType.Quiet,
+        "/APP_UNINSTALL_CODE" => ArgumentType.UninstallCode,
+        _ => throw new ArgumentException("Unknown argument was specified")
+    };
+}
+
+enum ArgumentType
+{
+    Uninstall,
+    Quiet,
+    UninstallCode
+}
+
+class Argument
+{
+    public Argument(ArgumentType argumentType, string data)
+    {
+        ArgumentType = argumentType;
+        Data = data;
+    }
+
+    public ArgumentType ArgumentType { get; set; }
+    public string Data { get; set; }
+}
